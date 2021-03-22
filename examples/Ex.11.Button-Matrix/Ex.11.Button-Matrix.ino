f4311a670fb1fa5e7a6bf2b43e51aca52d42cd6c
fredkh11 <fredkhamley@GMAIL.COM>
2021-03-22 00:21:48 +0000

    Update Ex.11.Button-Matrix.ino

--- examples/Ex.11.Button-Matrix/Ex.11.Button-Matrix.ino
+++ examples/Ex.11.Button-Matrix/Ex.11.Button-Matrix.ino
@@ -18,16 +18,19 @@ https://github.com/tttapa/MIDI_controller
 #include "MIDI_Controller.h" // Include the library
 
 const uint8_t velocity = 0b1111111; // Maximum velocity (0b1111111 = 0x7F = 127)
-const uint8_t addresses[4][3] = {   // the note numbers corresponding to the buttons in the matrix
-  {  1,  2,  3 },
-  {  4,  5,  6 },
-  {  7,  8,  9 },
-  { 10, 11, 12 }
-};
+const uint8_t addresses[14][8] = {   // the note numbers corresponding to the buttons in the matrix
+  {1,2,3,4,5,6,7,8,9,10,11,12,13,14},
+  {15,16,17,18,19,20,21,22,23,24,25,26,27,28},
+  {29,30,31,32,33,34,35,36,37,38,39,40,41,42},
+  {43,44,45,46,47,48,49,50,51,52,53,54,55,56},
+  {57,58,59,60,61,62,63,64,65,66,67,68,69,70},
+  {71,72,73,74,75,76,77,78,79,80,81,82,83,84},
+  {85,86,87,88,89,90,91,92,93,94,95,96,97,98}
+}
 
 // Create a new instance of the class 'ButtonMatrix', called 'buttonmatrix', with dimensions 4 rows and 3 columns, with the rows connected to pins 2, 3, 4 and 5
 // and the columns connected to pins 6, 7 and 8, that sends MIDI messages with the notes specified in 'addresses' on MIDI channel 1, with velocity 127
-ButtonMatrix<4, 3> buttonmatrix({2, 3, 4, 5}, {6, 7, 8}, addresses, 1, velocity);
+ButtonMatrix<14, 8> buttonmatrix({2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}, {15, 16, 17, 18, 19, 20, 21}, addresses, 1, velocity);
 
 void setup() {}
 
